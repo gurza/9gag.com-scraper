1a5a6b429025e41eec3e0bcba72e4156b35a68aa
Sergey Popinevskiy <gurza000@gmail.com>
2020-03-28 17:42:04 +0300

    Implement spider (#1)
    Implement the spider to scrape media posts from 9gag.com with infinite scrolling support.

--- ninegag_spider.py
+++ ninegag_spider.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
+import json
+
 import scrapy
 
 
 class NinegagSpider(scrapy.Spider):
-    pass
+    name = 'ninegag'
+    posts_base_url = 'https://9gag.com/v1/search-posts?query=%s&c=%s'
+
+    def __init__(self, query='', **kwargs):
+        self.count = 0
+        self.query = query
+        self.start_urls = [self.posts_base_url % (self.query, self.count)]
+        super().__init__(**kwargs)
+
+    def parse(self, response):
+        data = json.loads(response.body)
+
+        cnt = 0
+        for post in data.get('data', {}).get('posts', []):
+            cnt += 1
+
+            url = None
+            post_type = post.get('type')
+            if post_type == 'Photo':
+                url = post.get('images', {}).get('image700', {}).get('url')
+            elif post_type == 'Animated':
+                url = post.get('images', {}).get('image460sv', {}).get('url')
+
+            yield {
+                'id': post.get('id'),
+                'title': post.get('title'),
+                'url': url,
+            }
+
+        if cnt > 0:
+            self.count += cnt
+            yield scrapy.Request(self.posts_base_url % (self.query, self.count))
